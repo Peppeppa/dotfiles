9ab515ff80dd54b4634d3a9b72e2d1ad46246380
Stephen Eyring <accounts@eyring-s.de>
2024-06-09 13:26:30 +0200

    maus empfindlichkeit hochgesetzt

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 swayidle -w \
-timeout 120 ' swaylock ' \
+timeout 60 ' hyprlock ' \
 timeout 400 ' hyprctl dispatch dpms off' \
 timeout 12000 'systemctl suspend' \
 resume ' hyprctl dispatch dpms on' \
-before-sleep 'swaylock'
+before-sleep 'hyprlock'
